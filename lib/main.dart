88444064378fad189fa1dfb0329603231bd49c31
whgustj00 <whgustj00@naver.com>
2023-03-16 16:09:15 +0900

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,6 +15,7 @@ class App extends StatelessWidget {
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 40),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 height: 80,
@@ -42,6 +45,43 @@ class App extends StatelessWidget {
                   )
                 ],
               ),
+              SizedBox(
+                height: 120,
+              ),
+              Text('Total Balance',
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.8),
+                    fontSize: 22,
+                  )),
+              SizedBox(
+                height: 10,
+              ),
+              Text('\$ 5 942 182',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white.withOpacity(0.8),
+                    fontSize: 42,
+                  )),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 15,
+                        horizontal: 40,
+                      ),
+                      child: Text('Transfer', style: TextStyle(fontSize: 20)),
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),
